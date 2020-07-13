05d4f1f513c60ab862249f6b80e2d98b240a6608
Anders Björkland <anders@femtearenan.se>
2020-07-13 20:22:49 +0200

    updated how blog post can be edited with removal of images if needed

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -48,6 +48,7 @@ class BlogController extends AbstractController
                 $blogImageEntity = new BlogImage();
 
                 $imageFile = $blogImage->get('image')->getData();
+                dump($imageFile);
                 $alt = $blogImage->get('alt')->getData();
                 $subtext = $blogImage->get('subtext')->getData();
 
@@ -137,28 +138,123 @@ class BlogController extends AbstractController
     /**
      * @Route("/news/{slug}/edit", name="news_edit", methods={"GET", "POST"})
      */
-    public function editPost(BlogPost $blogPost, Request $request)
+    public function editPost(BlogPost $blogPost, Request $request, SluggerInterface $slugger)
     {
-        $form = $this->createForm(BlogPostType::class, $blogPost);
-
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-
-        }
+        $messages = [];
 
         $blogImages = $blogPost->getBlogImages();
         foreach ($blogImages as $blogImage) {
             $image = $blogImage->getImage();
             $alt = $image->getAlt();
-            dump($image);
         }
 
-        dump($blogPost, $blogImages);
+        $form = $this->createForm(BlogPostType::class, $blogPost);
+
+        $form->handleRequest($request);
+
+
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entitymanager = $this->getDoctrine()->getManager();
+            // make sure no empty blog image is being added
+
+            $blogInputs = $request->request->get("blog_post");
+            $hasBlogImages = array_key_exists("blogImages", $blogInputs);
+            if ($hasBlogImages) {
+                $blogInputs = $blogInputs["blogImages"];
+            }
+            dump($blogInputs, $request);
+            $currentBlogImages = $form->get('blogImages');
+            $prevBlogImages = $blogPost->getBlogImages();
+
+            $filePath = $_SERVER['APP_ENV'] === 'dev' ? 'images' : $this->getParameter('images_view');
+
+            // Update or remove images
+            foreach ($prevBlogImages as $prevBlogImage) {
+                $remove = true;
+                foreach ($blogInputs as $currentBlogImage) {
+                    if ($hasBlogImages && $prevBlogImage->getImage()->getFileName() === $currentBlogImage['image']) {
+                        $remove = false;
+
+                        // check if something needs updating
+                        $alt = $currentBlogImage['alt'];
+                        $subtext = $currentBlogImage['subtext'];
+
+                        $prevBlogImage->getImage()->setAlt($alt);
+                        $prevBlogImage->setSubtext($subtext);
+                        $entitymanager->persist($prevBlogImage);
+                        break;
+                    }
+                }
+
+                if ($remove) {
+                    $deleteStatus = unlink($filePath . '/' . $prevBlogImage->getImage()->getFileName());
+                    if ($deleteStatus) {
+                        $blogPost->removeBlogImage($prevBlogImage);
+                        $entitymanager->remove($prevBlogImage->getImage());
+                        $entitymanager->remove($prevBlogImage);
+                    } else {
+                        $messages[] = "Could not remove file " . $prevBlogImage->getImage()->getFileName();
+                    }
+                }
+            }
+
+            // insert new images
+            foreach ($currentBlogImages as $currentBlogImage) {
+                $isNew = true;
+                $currentFile = $currentBlogImage->get('image')->getData();
+
+                foreach ($prevBlogImages as $prevBlogImage) {
+                    if ($currentFile === $prevBlogImage->getImage()->getFileName()) {
+                        $isNew = false;
+                        break;
+                    }
+                }
+
+                if ($isNew && !empty($currentFile)) {
+                    $blogImageEntity = new BlogImage();
+                    $alt = $currentBlogImage->get('alt')->getData();
+                    $subtext = $currentBlogImage->get('subtext')->getData();
+
+                    $originalFilename = pathinfo($currentFile->getClientOriginalName(), PATHINFO_FILENAME);
+                    $safeFilename = $slugger->slug($originalFilename);
+                    $newFilename = $safeFilename.'-'.uniqid().'.'.$currentFile->guessExtension();
+
+                    try {
+                        $currentFile->move(
+                            $this->getParameter('images_directory'),
+                            $newFilename
+                        );
+                    } catch (FileException $e) {
+                        // ... handle exception if something happens during file upload
+                        $errors[] = $e;
+                    }
+
+                    $image = new Image();
+                    $image->setFileName($newFilename);
+                    $image->setAlt($alt);
+                    $entitymanager->persist($image);
+
+                    if (!empty($subtext)) {
+                        $blogImageEntity->setSubtext($subtext);
+                    }
+
+                    $blogImageEntity->setImage($image);
+                    $blogImageEntity->setBlogPost($blogPost);
+                    $entitymanager->persist($blogImageEntity);
+
+                    $blogPost->addBlogImage($blogImageEntity);
+                }
+            }
+
+            $entitymanager->flush();
+        }
+
 
         return $this->render('blog/edit.html.twig', [
             "form" => $form->createView(),
-            "blogPost" => $blogPost
+            "blogPost" => $blogPost,
+            "messages" => $messages
         ]);
     }
 }
