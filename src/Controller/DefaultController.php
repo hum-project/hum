d0fa176b9717ecafa6046898779cb5c512b1a34a
Anders Björkland <git@andersbjorkland.online>
2020-09-16 19:50:35 +0200

    Added example route to submit answers to.

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -4,16 +4,31 @@ namespace App\Controller;
 
 use App\Repository\BlogPostRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class DefaultController extends AbstractController
 {
     /**
-     * @Route("/", name="start")
+     * @Route("/", name="start", methods={"GET"})
      */
     public function index(BlogPostRepository $blogPostRepository)
     {
+        $blogPosts = $blogPostRepository->getPostsByLanguageName('English');
+        $pages = $blogPostRepository->getPageCountByLanguageName('English');
+        return $this->render('default/index.html.twig', [
+            'blogPosts' => $blogPosts,
+            'currentPage' => 1,
+            'pages' => $pages
+        ]);
+    }
+
+    /**
+     * @Route("/", name="submit_start", methods={"POST"})
+     */
+    public function submit(BlogPostRepository $blogPostRepository, Request $request)
+    {
+        dump($request);
         $blogPosts = $blogPostRepository->getPostsByLanguageName('English');
         $pages = $blogPostRepository->getPageCountByLanguageName('English');
         return $this->render('default/index.html.twig', [
