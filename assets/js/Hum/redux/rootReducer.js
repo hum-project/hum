ba5d6f75dd88da794becd76796e2a51fc7aac538
Anders Björkland <git@andersbjorkland.online>
2020-09-26 14:21:42 +0200

    refactored away client side to a different repo.

--- assets/js/Hum/redux/rootReducer.js
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from 'redux';
-import contentReducer from './contentReducer';
-import asyncReducer from "./asyncReducer";
-
-export default combineReducers({
-    contentReducer,
-    asyncReducer
-    // add any number of reducers that you have
-});
